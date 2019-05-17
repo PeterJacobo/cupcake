4d342563715e7414d8efe6d7abd65997c254d425
Peter Jacobo <object.opera@gmail.com>
2019-05-16 23:22:03 -0400

    Laravel 5.7 From Scratch - Blade Layout Files

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+    <title>@yield('title', 'Cupcake')</title>
+</head>
+<body>
+@yield('content')
+<ul>
+    <li><a href="/">Home</a></li>
+    <li><a href="/about">About Us</a></li>
+    <li><a href="/contact">Contact</a></li>
+</ul>
+</body>
+</html>
